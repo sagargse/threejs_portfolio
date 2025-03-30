5ea4be27066d3775bfd68582c98f264e1adb7bce
sagarraj <sagarraj200008@gmail.com>
2025-03-30 10:21:08 +0530

    add social media links

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -6,13 +6,13 @@ const Footer = () =>{
         </div>
         <div className="flex gap-3">
             <div className="social-icon">
-                <img src="/assets/github.svg" alt="github" className="w-1/2 h-1/2"/>
+                <a href="https://github.com/sagargse" className="flex items-center justify-center"><img src="/assets/github.svg" alt="github" className="w-1/2 h-1/2"/></a>
             </div>
             <div className="social-icon">
-                <img src="/assets/linkedin.svg" alt="github" className="w-1/2 h-1/2"/>
+                <a href="https://www.linkedin.com/in/sagar-raj-a910711b2/" className="flex items-center justify-center"><img src="/assets/linkedin.svg" alt="github" className="w-1/2 h-1/2"/></a>
             </div>
             <div className="social-icon">
-                <img src="/assets/twitter.svg" alt="github" className="w-1/2 h-1/2"/>
+                <a href="https://x.com/sagarastic_" className="flex items-center justify-center"><img src="/assets/twitter.svg" alt="github" className="w-1/2 h-1/2"/></a>
             </div>
         </div>
         <p className="text-white-500">© 2025 Sagar Raj. All rights reserved.</p>
